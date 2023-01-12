69d3180df95f3fa9301951351408403e41163027
Lukasye <yun.ye.lukas@gmail.com>
2023-01-12 21:49:56 +0100

    gms_test batch

---
+++ Code/gms_test.bat
@@ -0,0 +1,5 @@
+echo off
+start cmd /k python Server.py 
+start cmd /k python Server.py --port 10005 --opt 0
+start cmd /k python Server.py --port 10010 --opt 0
+start cmd /k python Server.py --port 10015 --opt 0
